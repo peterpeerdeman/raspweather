00d8db60ad9d4aec4c05d72ded6f4bea59e4f05f
Peter Peerdeman <peterpeerdeman@gmail.com>
2017-06-05 23:37:15 +0200

    feat(temp): remove inside temperature, use dotfiles for variables

--- temperature.js
+++ temperature.js
@@ -1,55 +1,27 @@
-var serialport = require('serialport');
-var SerialPort = require('serialport').SerialPort
 var request = require('request');
 var fs = require('fs');
-var serialPort = new SerialPort('/dev/serial/by-id/usb-Arduino__www.arduino.cc__Arduino_Uno_649363339363515081E1-if00', {
-    baudrate: 115200,
-    parser: serialport.parsers.readline('\n')
-}, false); // this is the openImmediately flag [default is true]
 var express = require('express');
 var app = express();
+var CronJob = require('cron').CronJob;
 
-var lastTimeStamp = new Date();
-
-var PARTICLE_DEVICE_ID = '';
-var PARTICLE_ACCESS_TOKEN = '';
-
-serialPort.open(function(error) {
-    if (error) {
-        console.log('failed to open: ' + error);
-    } else {
-        console.log('now logging temperatures to file');
-        serialPort.on('data', function(data) {
-            var stringData = data.toString();
-            var split = stringData.split(':');
-            if (split.length > 0) {
-                var temperatureFloat = parseFloat(split[1]);
-                if (temperatureFloat) {
-                    var now = new Date();
-                    if (now - lastTimeStamp > 1000 * 60 * 60) {
-                        var logEntry = new Date().toString() + ';' + temperatureFloat + '\n';
-                        fs.appendFile('temperatures.txt', logEntry, function(err) {
-                            //
-                        });
-                        logOutsideTemperature();
-                        console.log(temperatureFloat);
-                        lastTimeStamp = now;
-                    }
-                }
-            }
-        });
-    }
+var logOutsideTemp = new CronJob({
+    cronTime: '*/5 * * * *',
+    onTick: function() {
+        logOutsideTemperature();
+    },
+    start: true,
+    timeZone: 'Europe/Amsterdam'
 });
 
 function logOutsideTemperature() {
     var url = ['https://api.particle.io/v1/devices/',
-        PARTICLE_DEVICE_ID,
+        process.env.PARTICLE_DEVICE_ID,
         '/temperature?access_token=',
-        PARTICLE_ACCESS_TOKEN].join('');
+        process.env.PARTICLE_ACCESS_TOKEN].join('');
     request(url, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             var temperatureFloat = JSON.parse(body).result;
-            if (temperatureFloat == "-0.0625" || parseFloat(temperatureFloat) >100) return;
+            if (parseFloat(temperatureFloat) < -100 || temperatureFloat == "-0.0625" || parseFloat(temperatureFloat) >100) return;
             var logEntry = new Date().toString() + ';' + temperatureFloat + '\n';
             fs.appendFile('temperatures-outside.txt', logEntry, function(err) {
                 //
